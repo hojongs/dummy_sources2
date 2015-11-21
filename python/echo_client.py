59858da869e46a7b445db388b055a399677ccc79
ssaemo <jeontha@gmail.com>
2015-11-20 20:10:21 -0800

    finish

--- python/echo_client.py
+++ python/echo_client.py
@@ -9,10 +9,11 @@ port = 6666
 sock = socket(AF_INET, SOCK_STREAM)
 sock.connect((host,port))
 while True:
+	print "input : ",
 	buf=raw_input()
 	sock.send(buf)
 	if not buf:
 		break
 	buf = sock.recv(1024)
+	print "receive : "+buf
 sock.close()
-print buf
